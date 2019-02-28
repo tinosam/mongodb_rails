7a16ecb31f0d6f2dc320971f8e3ea45186d6a9a4
tinosam <yvestinos@gmail.com>
2019-03-01 00:46:02 +0300

    first push

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :users
+  resources :movies
+
+  root "movies#index"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
